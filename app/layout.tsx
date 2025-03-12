55cf87ff159308ff5ab8d6cb2e132e14424173c5
joaovictoralmeida <124613767+joaovictoralmeida@users.noreply.github.com>
2025-03-12 16:33:02 -0300

    att

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
       >
         <NavBar/>
         {children}
-        <Footer/>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Login from "./components/Login";
 
 export default function Home() {
   return (
     <>
      <Login/>
+     <Footer/>
     </>
   );
 }
